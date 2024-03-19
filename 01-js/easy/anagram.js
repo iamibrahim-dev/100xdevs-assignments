93f3591841a36ebddab0104a79c6a087d9c59a8b
iamibrahim-dev <ibrahim665@outlook.in>
2024-03-19 12:20:39 +0530

    Solution for evaluating whether the given two strings are anagram or not

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,33 @@
 */
 
 function isAnagram(str1, str2) {
+  let n1 = str1.length;
+  let n2 = str2.length;
 
+  if (n1 != n2) {
+    return false;
+  }
+
+  let str1Array = [];
+  let str2Array = [];
+
+  for (let i = 0; i < str1.length; i++) {
+    str1Array.push(str1[i].toLowerCase());
+  }
+
+  for (let i = 0; i < str2.length; i++) {
+    str2Array.push(str2[i].toLowerCase());
+  }
+
+  str1Array.sort();
+  str2Array.sort();
+
+  for (let i = 0; i < str1Array.length; i++) {
+    if (str1Array[i] != str2Array[i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 module.exports = isAnagram;
